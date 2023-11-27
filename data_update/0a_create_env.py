fdbae20590a77bff618790df24fd7d8a001becb7
Ariel Saffer <61440835+arielsaffer@users.noreply.github.com>
2023-11-27 16:42:46 -0500

    update database fields

--- data_update/0a_create_env.py
+++ data_update/0a_create_env.py
@@ -7,8 +7,8 @@ Description: Create the .env file with workspace-relevant information
 
 # Where the .csv files are being stored (data_dir)
 
-drive_letter = "Q:"
-data_dir = "/Shared drives/Pandemic Data/Invasive database/"
+drive_letter = "Y:" 
+data_dir = "/GITAR/Database/"
 
 # Auth token for EPPO API
 
@@ -25,13 +25,6 @@ eppo_report_last_update = "2023-04-14"
 
 with open(".env", "w") as f:
     f.write(f"DATA_PATH='{drive_letter + data_dir}'\n")
-    f.write(f"USERNAME='{username}'\n")
-    f.write(f"PASSWORD='{password}'\n")
-    f.write(f"HOST='{host}'\n")
-    f.write(f"PORT='{port}'\n")
-    f.write(f"DATABASE='{database}'\n")
-    f.write(f"DB_URL='{db_url}'\n")
-    f.write(f"TRY_URL='{try_url}'\n")
     f.write(f"EPPO_TOKEN='{eppo_token}'\n")
     f.write(f"BASE_OBS_YEAR='{base_obs_year}'\n")
     f.write(f"GBIF_OBS_UPDATED='{gbif_obs_last_update}'\n")
