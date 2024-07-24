8d5d5d8c152338b903b7e0e795ee6d54cc0da30b
Ariel Saffer <61440835+arielsaffer@users.noreply.github.com>
2024-07-23 20:46:20 -0400

    headers and function import

--- data_update/1a2_check_unfound_gbif_keys.py
+++ data_update/1a2_check_unfound_gbif_keys.py
@@ -1,3 +1,13 @@
+"""
+File: data_update/1a2_check_unfound_gbif_keys.py
+Author: Thom Worm
+Date created: 2024-06-01
+Description: This script checks for species that were not found in the GBIF backbone and 
+attempts to match them to the GBIF backbone using the pygbif package. It then updates the 
+species lists with the new GBIF keys and taxonomic information.
+"""
+
+
 import pandas as pd
 import os
 import sys
@@ -9,14 +19,12 @@ sys.path.append(os.getcwd())
 
 dotenv.load_dotenv(".env")
 data_dir = os.getenv("DATA_PATH")
-# data_dir = "G:\GITAR\PubData"
 
+# Import data functions
+from data_update.data_functions import check_gbif_tax_secondary, update_GBIFstatus
 
-# set data dir to "G:\GITAR\PubData"
-# data_dir = r"G:\GITAR\PubData\\"
 
 # Bring in new species lists
-
 
 invasive_all_source = pd.read_csv( data_dir + "\species lists\invasive_all_source.csv", dtype={"usageKey": str})
 gbif_backbone = pd.read_csv( data_dir + "\GBIF data\GBIF_backbone_invasive.csv", dtype={"usageKey": str})
@@ -35,9 +43,6 @@ sinas_gbif['matchType'] = sinas_gbif['matchType'].replace(np.nan, '', regex=True
 data_files = [sinas_gbif]
 test = [cabi_gbif.head(50).copy()]
 
-
-# Retry decorator to handle HTTP and timeout errors
-
 specie_unfound = []
 for file in data_files:
     for index, row in file.iterrows():
@@ -52,16 +57,6 @@ check_species_df['Taxon_orig'] = check_species_df['Taxon']
 matched_species, unmatched = check_gbif_tax_secondary(check_species_df)
 # make dc into dataframe
 matched_species = pd.DataFrame(matched_species)
-
-
-
-def update_GBIFstatus(row):
-    if row["GBIFstatus"] == "Missing" and row["GBIFstatus_Synonym"] != None:
-        row["GBIFstatus"] = row["GBIFstatus_Synonym"]
-    elif row["GBIFstatus"] == None and row["GBIFstatus_Synonym"] != None:
-        row["GBIFstatus"] = row["GBIFstatus_Synonym"]
-    return row
-
 
 matched_species = matched_species.apply(update_GBIFstatus, axis=1)
 # write matched_species to csv as previously_unmatched_species.csv
@@ -126,7 +121,6 @@ for file in data_files:
             file.at[index, "usageKey"] = search['GBIFusageKey']
             file.at[index, "canonicalName"] = search['Taxon']
             file.at[index, "scientificName"] = search['scientificName']
-            # file.at[index, "GBIFstatus"] = search['GBIFstatus']
             file.at[index, "matchType"] = search['GBIFmatchtype']
             file.at[index, "rank"] = search['GBIFtaxonRank']
             file.at[index, "taxonomic_species"] = search['species']
@@ -135,12 +129,7 @@ for file in data_files:
             search = gbif_backbone[gbif_backbone['usageKey'] == row['usageKey']]
 
             if search.empty:
-                # print(row['usageKey'])
-                # break
-                # print(row)
-                # print(row["species"])
                 print('no match to gbif backbone')
-                # print(row['usageKey'])
                 if (
                     pd.isnull(file.at[index, "usageKey"])
                     or file.at[index, "usageKey"] == ""
@@ -173,13 +162,9 @@ for file in data_files:
                 if pd.isnull(row['canonicalName']):
                     file.at[index, "canonicalName"] = search['species']
                     file.at[index, "scientificName"] = search['scientificName']
-                    # file.at[index, "GBIFstatus"] = search['GBIFstatus']
                     file.at[index, "matchType"] = search['taxonomicStatus']
                     file.at[index, "rank"] = search['taxonRank']
                     file.at[index, "taxonomic_species"] = search['species']
-            # print file at index
-            # print(file.at[index, "kingdom"])
-            # if usageKey is na
 
         if pd.isnull(file.at[index, "usageKey"]) or file.at[index, "matchType"] == "HIGHERRANK" or file.at[index, "usageKey"] == "NA" or file.at[index, "usageKey"] == "" or pd.isnull(row['usageKey']):
             print("making XX")
@@ -199,18 +184,14 @@ for file in data_files:
             print(file.at[index, "species"])
         else:
             print(row['usageKey'])
-            #print('broke')
-
-
-
 
 cabi_gbif_match = data_files[0]
 eppo_gbif_match = data_files[1]
 daisie_gbif_match = data_files[2]
 sinas_gbif_match = data_files[3]
 
-#write to csv 
-cabi_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\cabi_gbif_matched.csv")
-eppo_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\eppo_gbif_matched.csv")
-daisie_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\daisie_gbif_matched.csv")
-sinas_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\sinas_gbif_matched.csv")
+# write to csv 
+cabi_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\cabi_gbif.csv")
+eppo_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\eppo_gbif.csv")
+daisie_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\daisie_gbif.csv")
+sinas_gbif_match.to_csv(data_dir + "\species lists\gbif_matched\sinas_gbif.csv")
