c32ceb724df78ee37e7a2720d6bc816f3b3434b6
Ariel Saffer <61440835+arielsaffer@users.noreply.github.com>
2025-05-15 15:16:42 -0400

    move to pyproject.toml

--- query_functions/python/GIATAR_query_functions.py
+++ query_functions/python/GIATAR_query_functions.py
@@ -7,9 +7,7 @@ Description: Functions to facilitate querying the GIATAR database
 
 import pandas as pd
 import numpy as np
-import math
 import os
-import warnings
 import pygbif
 import dotenv
 import requests
